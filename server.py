36f60350de2a2d2b4c264fde2708c988007c025c
JIA YEE <jiayeerawr@gmail.com>
2017-09-22 08:37:06 -0700

    Initial commit
    Signed-off-by: JIA YEE <jiayeerawr@gmail.com>

---
+++ server.py
@@ -0,0 +1,9 @@
+from os import environ
+from gevent.wsgi import WSGIServer
+from app import app
+
+if __name__ == '__main__':
+    port = int(environ.get('PORT', 5000))
+    http_server = WSGIServer(('', port), app)
+    http_server.serve_forever()
+
