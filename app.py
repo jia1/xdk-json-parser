f1ff5fe14d0831b75ec5a3a108e88618941074aa
JIA YEE <jiayeerawr@gmail.com>
2017-10-02 01:17:47 -0700

    Get bosch xdk sensor data
    Signed-off-by: JIA YEE <jiayeerawr@gmail.com>

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import requests
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return requests.get('http://52.187.51.158:8082/mongodb/boschxdk03/latestdata').content
 
