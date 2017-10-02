d6590fb98957a700d23ac17c5b20ac88de1c4799
JIA YEE <jiayeerawr@gmail.com>
2017-10-02 01:31:44 -0700

    Return filtered json response
    Signed-off-by: JIA YEE <jiayeerawr@gmail.com>

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import requests
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return requests.get('http://52.187.51.158:8082/mongodb/boschxdk03/latestdata').content
+    data = requests.get('http://52.187.51.158:8082/mongodb/boschxdk03/latestdata').json()
+    return jsonify({key:value for key,value in data.items() if key in ['noiselevel','temperature','humidity','millilux']})
 
